c41268dace0177f7f9b23aa646d22552159c9c3f
valmac <valmac1@yandex.ru>
2019-01-12 17:08:26 +0300

    update shell

--- conf/prod/orders-fix42.sh
+++ conf/prod/orders-fix42.sh
@@ -3,6 +3,8 @@
 APP=orders-fix42
 OUT_FILE=$APP.out
 PID_FILE=$APP.pid
+CFG_DIR=~/go/src/github.com/bitfinexcom/bfxfixgw/conf/prod/service
+#CFG_FILE="orders_fix42.cfg"
 
 function welcome(){
     echo "====================================="
@@ -23,7 +25,8 @@ welcome
 case $1 in
   start)
     echo "start.."    
-      FIX_SETTINGS_DIRECTORY=~/go/src/github.com/bitfinexcom/bfxfixgw/conf/integration_test/service ~/go/bin/bfxfixgw -v -orders -ordcfg "orders_fix42.cfg" -rest "https://api.bitfinex.com/v2/" -ws "wss://api.bitfinex.com/ws/2" &       
+    export FIX_SETTINGS_DIRECTORY=$CFG_DIR
+    ~/go/bin/bfxfixgw -orders -ordcfg "orders_fix42.cfg" -rest "https://api.bitfinex.com/v2/" -ws "wss://api.bitfinex.com/ws/2" &       
     echo $! > $PID_FILE
     get_pid
     echo "PID = '$PID'"
@@ -33,23 +36,26 @@ case $1 in
   stop)
     echo "stop.."       
     get_pid
-    echo "    pid=$PID"
+    echo "pid=$PID"
     PROCESS_EXISTS=`ps -ax | grep $PID | grep -v grep|wc -l`
     if [ $PROCESS_EXISTS == "0" ]; then
       echo "ERROR : PROCESS NOT FOUND BY PID ($PID)"
       exit 1
     fi
     
-    echo "kill proces_id: $PID"
+    echo "kill pid: $PID"
     kill -9 $PID
+    echo "remove $PID_FILE.."
     rm $PID_FILE 
     ;;
 
   status)
     echo "status.."
+    echo "config:      $CFG_DIR"
+    echo "config file: $CFG_FILE"
     get_pid
-    echo "pidfile: $PID_FILE"
-    echo "pid:     $PID"
+    echo "pidfile:     $PID_FILE"
+    echo "pid:         $PID"
     echo ""
     
     echo "grep search process: 'bfxfixgw'.."
