207932a19a7c8b8118f17c2ecda8eb92978dadb1
valmac <valmac1@yandex.ru>
2019-01-13 18:44:29 +0300

    add to manager run command-start with intercative console output

--- dockers/bfx-orders-fix42/manager.sh
+++ dockers/bfx-orders-fix42/manager.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
+HOST_IP=127.0.0.1
+PORT=5002
 NAME=bfx-orders-fix42
 IMAGE=$NAME:latest
 MOUNT_PATH=/path_to_fix_configs
 
 
-function getID(){
-  echo "get id.."
+function getID(){  
   docker inspect --format="{{.Id}}" $NAME
 }
 function start(){
-  echo "start.."
-  docker run -itd --volume $MOUNT_PATH:/fix_settings --name $NAME $IMAGE
+  echo "[start].."
+  docker run -itd --volume $MOUNT_PATH:/fix_settings -p $HOST_IP:$PORT:5002 -e DEBUG=1  --name $NAME $IMAGE
+}
+function run(){
+  echo "[run].."
+  docker run -it --volume $MOUNT_PATH:/fix_settings -p $HOST_IP:$PORT:5002 -e DEBUG=1  --name $NAME $IMAGE
 }
 function stop(){
-  echo "stop.."
+  echo "[stop].."
   docker stop $NAME
 }
 function rm(){
-  echo "remove.."
+  echo "[remove].."
   docker rm $(getID)
 }
 function pull(){
-  echo "pull.."
+  echo "[pull].."
   docker pull $IMAGE
 }
 function logs(){
-  echo "logs.."
+  echo "[logs].."
   docker logs $NAME
 }
 function inspect(){
-  echo "inspect.."
+  echo "[inspect].."
   docker inspect $NAME
 }  
 case $1 in
   start)
     start
+    ;;
+  run)
+    run
     ;;
  
   stop)
@@ -87,10 +95,27 @@ case $1 in
     echo "       NAME       = $NAME"
     echo "       IMAGE      = $IMAGE"
     echo "       MOUNT_PATH = $MOUNT_PATH"
+    echo "       HOST_IP    = $HOST_IP"
+    echo "       PORT       = $PORT"
     ;;
-
+  help)
+    echo "[help] commands:"
+    echo "    start      start new container (detached mode)"
+    echo "    run        start new container (interactive mode)"
+    echo "    stop	 stop runned container if exists"
+    echo "    stop-rm    stop and remove runned container if exists"
+    echo "    rm	 remove exist container"
+    echo "    upgrade    stop remove pull start contaner"
+    echo "    pull       pull image"
+    echo "    logs       show logs of started container"
+    echo "    inspect    inspect to container"
+    echo "    id         print id"
+    echo "    name       print name"
+    echo "    image      print image id"
+    echo "    config     print env"
+    ;;
   *)
-    echo "usage: $NAME.sh {start|stop|stop-rm|rm|upgrade|pull|logs|inspect|id|name|image|config}" 
+    echo "usage: $NAME.sh {start|run|stop|stop-rm|rm|upgrade|pull|logs|inspect|id|name|image|config}" 
     ;;    
 esac
 exit 0
